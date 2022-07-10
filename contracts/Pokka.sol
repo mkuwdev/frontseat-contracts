560b952fe96fa20a2b7737789fcceb05ec80ac25
[your <alessandrofigo24@gmail.com>
2022-07-10 23:25:03 +0700

    minor

--- contracts/Pokka.sol
+++ contracts/Pokka.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol"
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract Pokka is ReentrancyGuard {
-    
+    bool hello = false;
 }

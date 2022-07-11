cbf985e667488162def4d6e54c75bb439c8ecbc0
[your <alessandrofigo24@gmail.com>
2022-07-11 13:37:01 +0700

    no set name

--- contracts/Pokka.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
-
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-
-contract Pokka is ReentrancyGuard {
-    bool hello = false;
-}
